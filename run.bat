e857acfd98016e1a9900f61c4c8f941b818587d3
温婧 <j-wen15@mails.tsinghua.edu.cn>
2017-11-17 21:12:56 +0800

    run.bat

---
+++ run.bat
@@ -0,0 +1,2 @@
+g++ -c pf/pf_buffermgr.cc pf/pf_error.cc pf/pf_filehandle.cc pf/pf_hashtable.cc pf/pf_manager.cc pf/pf_pagehandle.cc pf/pf_statistics.cc utils/statistics.cc rm/rm_filehandle.cc rm/rm_manager.cc rm/rm_record.cc rm/rm_rid.cc rm/rm_filescan.cc rm/rm_manager.cc rm/rm_test.cc -DPF_STATS
+g++ -o main pf_buffermgr.o pf_error.o pf_filehandle.o pf_hashtable.o pf_manager.o pf_pagehandle.o pf_statistics.o pf_test2.o statistics.o
